331efb45d7e58ec249fc1d7b6611c3efaaf9d42a
shabahat97 <shabahatalichishti@gmail.com>
2021-08-20 20:47:30 +0500

    Add project files.

---
+++ CustomWebService/Script.js
@@ -0,0 +1,36 @@
+﻿var app = angular
+    .module("myModule", [])
+    .controller("myController", function ($scope, stringService) {
+     
+        //two problems here:
+        //1) controller code is big large and complex
+        //2)if we want to reuse logic we have to duplicate that code in that controller
+
+        //solution create  service
+        $scope.transformString = function (input) {
+
+            //if (!input) {
+
+
+            //    return input;
+            //}
+
+            //var output = "";
+
+            //for (var i = 0; i < input.length; i++) {
+
+            //    //skip first letter
+            //    if (i > 0 && input[i] == input[i].toUpperCase()) {
+
+            //        output = output + " ";
+            //    }
+            //    // else continue appending characters to output
+            //    output = output + input[i];
+            //}
+
+            $scope.output = stringService.processString(input);
+
+        }
+    });
+        
+    
---
+++ CustomWebService/stringService.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="script.js" />
+
+//to create custom service and register with angular we use factory method
+app.factory('stringService', function () {
+    //this service return javascript object
+    return
+    {
+        processString: (function (input) {
+            if (!input) {
+
+
+                return input;
+            }
+
+            var output = "";
+
+            for (var i = 0; i < input.length; i++) {
+
+                //skip first letter
+                if (i > 0 && input[i] == input[i].toUpperCase()) {
+
+                    output = output + " ";
+                }
+                // else continue appending characters to output
+                output = output + input[i];
+            }
+            return output;
+
+
+        })();
+    };
+});
